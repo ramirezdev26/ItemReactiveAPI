db9308706facc0291fd51c9cd87487e6ae4a74bc
Santiago Ramirez Cano <santiago.ramirezc26@gmail.com>
2023-03-21 18:54:45 -0500

    Project created with dependencies needed such as the configuration with MongoAtlas

---
+++ src/main/java/co/com/item/reactive/api/ItemReactiveAPI/ItemReactiveApiApplication.java
@@ -0,0 +1,13 @@
+package co.com.item.reactive.api.ItemReactiveAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemReactiveApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemReactiveApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/item/reactive/api/ItemReactiveAPI/ItemReactiveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.item.reactive.api.ItemReactiveAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemReactiveApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
