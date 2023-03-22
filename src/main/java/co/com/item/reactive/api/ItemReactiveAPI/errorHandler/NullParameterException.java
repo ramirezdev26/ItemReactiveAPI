104b01feb197b23676ad3832b847701c81f9bba3
Santiago Ramirez Cano <santiago.ramirezc26@gmail.com>
2023-03-21 19:34:38 -0500

    Created the Model mapper config, such as the Null Parameter Exception

---
+++ src/main/java/co/com/item/reactive/api/ItemReactiveAPI/errorHandler/NullParameterException.java
@@ -0,0 +1,7 @@
+package co.com.item.reactive.api.ItemReactiveAPI.errorHandler;
+
+public class NullParameterException extends RuntimeException {
+    public NullParameterException(String message) {
+        super(message);
+    }
+}
